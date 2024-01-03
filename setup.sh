3bc347c1f3e8bc3248b528727e16d6169ed2e61f
zilencer <zilencer@protonmail.com>
2024-01-03 16:26:04 -0600

    Moved container-setup.sh to setup.sh

---
+++ setup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+SUBNET="192.168.20.0/24"
+GATEWAY="192.168.20.1"
+IP="192.168.20.253/32"
+IP_ADDR="192.168.20.25"
+INTERFACE="enp0s25"
+
+abnormal_exit() {
+    echo "Error: $1"
+    exit 1
+}
+
+[ $(whoami) != "root" ] && abnormal_exit "Permission Denied"
+
+printf "Building podman image ..."
+if [[ ! $(podman images | grep -o krb5-srv) ]]; then
+   podman build -q -t krb5-srv -f "$(pwd)/config/Dockerfile"
+else
+   printf "exists, skipping\n" 
+fi
+
+#printf "Creating podman network ..."
+#if [[ ! $(podman network ls | grep -i local-lan) ]]; then
+#   podman network create \
+#      -d ipvlan \
+#      --subnet $SUBNET \
+#      --gateway $GATEWAY \
+#      --ip-range $IP \
+#      -o parent=$INTERFACE \
+#      --ignore \
+#      local-lan
+#else
+#   printf "exists, skipping\n"
+#fi
+
+#printf "Creating podman container ..." 
+#if [[ ! $(podman ps -a | grep -o kdc) ]]; then
+#   podman run -d -q \
+#     --ip $IP_ADDR \
+#     --name kdc \
+#     --hostname kdc \
+#     --network local-lan \
+#     localhost/krb5-srv
+#elif [[ ! $(podman ps | grep -i kdc) ]]; then
+#   podman start kdc
+#   printf "exists, starting\n"
+#fi
+
+#podman exec -it kdc \
+#   usr/local/bin/krb5-kdc.sh \
+#   -h kerberos -i $IP_ADDR -s host \
+#   -h media-srv -i 192.168.20.10 -s nfs \
+#   -h file-srv -i 192.168.20.11 -s nfs \
+#   -d HOME.LOCAL
+
